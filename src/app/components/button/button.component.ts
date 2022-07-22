efd4d4ce59f1ca08d4a76f81e2369c21e0647855
m.osumi <m.osumi@goodpatch.com>
2022-07-22 14:07:03 +0900

    :+1: add click event of button

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ButtonComponent implements OnInit {
   @Input() label: string = 'Hello';
   @Input() priority: 'primary' | 'secondary' | 'normal' = 'normal';
+  @Output() onClick = new EventEmitter<Event>();
 
   constructor() {}
 
